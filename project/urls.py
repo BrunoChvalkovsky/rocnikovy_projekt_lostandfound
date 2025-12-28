7f1c1c27cffb42ca72a70b8b94a38ccbf1f94345
BrunoChvalkovsky <bruno.chvalkovsky@gmail.com>
2025-12-28 13:21:54 +0100

    add homeage

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import lostandfound.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lostandfound.views.homepage, name='homepage'),
 ]
