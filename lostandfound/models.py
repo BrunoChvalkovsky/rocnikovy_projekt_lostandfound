98b48c372b3d73a207f698ed6a96188511f3081a
BrunoChvalkovsky <bruno.chvalkovsky@gmail.com>
2025-12-28 14:12:21 +0100

    gitignore update, models added

--- lostandfound/models.py
+++ lostandfound/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    contact = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+    title = models.CharField(max_length=80)
+    description = models.TextField()
+    image = models.ImageField(upload_to="post_images/")
+
+    def __str__(self):
+        return self.title
